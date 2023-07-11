e264382c84871c6ec6f36c49a00b35676a2acb19
faisal khan <dev.faisalkhan@gmail.com>
2023-07-11 22:31:25 +0500

    fixed ( check constrain failed )

--- intellishuff-api/src/institution/institution.entity.ts
+++ intellishuff-api/src/institution/institution.entity.ts
@@ -1,7 +1,6 @@
-import {  Column, Entity, OneToOne } from "typeorm";
+import {  Column, Entity, JoinColumn, OneToOne } from "typeorm";
 import { BaseEntity } from "../universal/entity/base.entity";
 import { InstitutionType } from "./institution.model";
-import { User } from "src/user/user.entity";
 
 @Entity()
 export class Institution extends BaseEntity {
@@ -9,12 +8,6 @@ export class Institution extends BaseEntity {
     @Column()
     name: string;
 
-    @Column({ type: 'blob', nullable: true })
-    image: Buffer
-
-    @Column({enum: InstitutionType})
+    @Column()
     type: InstitutionType;
-
-    // @OneToOne(() => User, user => user.institution)
-    // user: User;
 }

1b1bca9b6cd3cc99309b85310ef8765ad0152fdf
faisal khan <dev.faisalkhan@gmail.com>
2023-07-19 03:47:02 +0500

    Update user.module.ts

--- intellishuff-api/src/user/user.module.ts
+++ intellishuff-api/src/user/user.module.ts
@@ -6,10 +6,12 @@ import { User } from './user.entity';
 import { InstitutionModule } from 'src/institution/institution.module';
 import { Institution } from 'src/institution/institution.entity';
 import { HelperService } from 'src/universal/helper.service';
+import { Role } from 'src/role/role/role.entity';
+import { RoleService } from 'src/role/role/role.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User, Institution]),
+    TypeOrmModule.forFeature([User, Institution, Role]),
     InstitutionModule
   ],
   controllers: [UserController],

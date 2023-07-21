897554cdfb77deac18f7320d5855ef1382794499
faisal khan <dev.faisalkhan@gmail.com>
2023-07-21 21:01:01 +0500

    added custom fonts

--- intellsihuff-pwa/src/app/universal/app-constant.ts
+++ intellsihuff-pwa/src/app/universal/app-constant.ts
@@ -1,17 +1,17 @@
-import { environment } from "src/environments/environment.prod";
-
+import { environment } from "src/environments/environment";
 
 export class AppConstant {
     public static readonly DEBUG = !environment.production;
 
     public static readonly BASE_URL = !environment.production 
-        ? "https://intellishuf.dotnetdreamer2.net/api/v1/" : "http://localhost:3000/api/v1/";
+        ? "http://localhost:3000/api/v1/" : "https://intellishuf.dotnetdreamer2.net/api/v1/"; 
+
     public static readonly BASE_API_URL = `${AppConstant.BASE_URL}`;
     public static readonly DB_NAME = "intellishuff";
     public static readonly GOOGLE_MAP_API_KEY = 'AIzaSyD0y6Q6UmumKiMplydd-jynVkXkeOFH7WY'; 
 
     // public static readonly DEFAULT_DATETIME_FORMAT = "YYYY-MM-DD HH:mm:ss";
-    public static readonly DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    public static readonly DATETIME_FORMAT = "YYYY-MM-DD HH:mm:ss.SSS";
     public static readonly DATE_FORMAT = "yyyy-MM-dd";
     public static readonly TIME_FORMAT = "HH:mm";
     public static readonly TIME_FORMAT_AMPM = "hh:mm a";

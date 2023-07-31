a6eb59976246860f3d6c1317ee2a84054c8e6cc0
faisal khan <dev.faisalkhan@gmail.com>
2023-07-31 20:45:14 +0500

    minor changes

--- intellsihuff-pwa/src/app/modules/authentication/edit-profile/edit-profile.component.ts
+++ intellsihuff-pwa/src/app/modules/authentication/edit-profile/edit-profile.component.ts
@@ -1,4 +1,3 @@
-
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IRegister, IRole, IUser, InstitutionType } from '../auth.model';
 import { AuthService } from '../auth.service';
@@ -8,11 +7,12 @@ import { AppConstant } from 'src/app/universal/app-constant';
 import { UserConstant } from '../../user/user-constant';
 import { BasePage } from 'src/app/universal/base.page';
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'edit-profile',
   templateUrl: './edit-profile.component.html',
-  styleUrls: ['./edit-profile.component.scss']
+  styleUrls: ['./edit-profile.component.scss'],
 })
 export class EditProfileComponent extends BasePage implements OnInit {
   id: number;
@@ -26,48 +26,46 @@ export class EditProfileComponent extends BasePage implements OnInit {
     return this.formGroup.controls;
   }
   constructor(
-    private formBuilder: FormBuilder
-    , private authSvc: AuthService
-    , private route: ActivatedRoute
+    private formBuilder: FormBuilder,
+    private authSvc: AuthService,
+    private route: ActivatedRoute,
+    private titleService: Title
   ) {
     super();
+    this.titleService.setTitle(`Edit Profile | ${AppConstant.SITE_NAME}` )
+
     this.formGroup = formBuilder.group({
       name: ['', Validators.required],
-      email: [{value:'', disabled: true}],
+      email: [{ value: '', disabled: true }],
       password: ['', Validators.required],
       institutionName: ['', Validators.required],
-      institutionType: [{value:'', disabled: true}]
+      institutionType: [{ value: '', disabled: true }],
     });
-    
   }
 
   async ngOnInit() {
-    this.route.queryParams.subscribe(params => {
+    this.route.queryParams.subscribe((params) => {
       this.id = params['id'];
     });
-    
-    if(this.id) {
+
+    if (this.id) {
       this.helperSvc.presentLoader('Fetching User');
       try {
         this.user = await this.authSvc.getCurrentUser(this.id);
-      
-        if(this.user) {
+
+        if (this.user) {
           this._populateFormGroup();
         }
       } catch (error) {
-        
       } finally {
         this.helperSvc.dismissLoader();
       }
-   
-    }    
+    }
 
-    if(AppConstant.DEBUG) {
+    if (AppConstant.DEBUG) {
       // this._preFill();
     }
   }
-
-
 
   async onFormSubmitted(data: IRegister) {
     // const loader = await this.helperSvc.loader;
@@ -75,44 +73,41 @@ export class EditProfileComponent extends BasePage implements OnInit {
     const institution = {
       name: this.fg['institutionName'].value,
       type: this.user.institution.type,
-    }
+    };
 
-    const params =  {
+    const params = {
       id: this.user.id,
       email: this.user.email,
       name: data.name,
       roles: this.user.roles,
       password: data.password,
       status: data.status,
-      institution: institution
-    }
+      institution: institution,
+    };
 
     this.helperSvc.presentLoader('Updating User');
 
     try {
       const resp: IResponse<any> = await this.authSvc.regsiter(params);
-      if(resp.status) {
-        await this.helperSvc.presentAlert(resp.message, 'success'); 
-        this.pubsubSvc.publishEvent(UserConstant.EVENT_USER_PROFILE_UPDATED , { ...resp.data } );
-        
+      if (resp.status) {
+        this.pubsubSvc.publishEvent(UserConstant.EVENT_USER_PROFILE_UPDATED, {
+          ...resp.data,
+        });
+        await this.helperSvc.presentAlert(resp.message, 'success');
       } else {
-        await this.helperSvc.presentAlert(resp.message, 'warning'); 
+        await this.helperSvc.presentAlert(resp.message, 'warning');
       }
-      
     } catch (error) {
-      
     } finally {
       this.helperSvc.dismissLoader();
     }
-    
   }
 
-  private  _populateFormGroup() {
-   this.fg['name'].setValue(this.user.name);
-   this.fg['email'].setValue(this.user.email);
-   this.fg['password'].setValue(this.user.password);
-   this.fg['institutionName'].setValue(this.user.institution?.name);
-   this.fg['institutionType'].setValue(this.user.institution?.type);
+  private _populateFormGroup() {
+    this.fg['name'].setValue(this.user.name);
+    this.fg['email'].setValue(this.user.email);
+    this.fg['password'].setValue(this.user.password);
+    this.fg['institutionName'].setValue(this.user.institution?.name);
+    this.fg['institutionType'].setValue(this.user.institution?.type);
   }
-
 }

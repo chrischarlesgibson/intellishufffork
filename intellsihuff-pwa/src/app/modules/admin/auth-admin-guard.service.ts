805a16c492491f7fb9e1d8812faafa66f3743bcd
faisal khan <dev.faisalkhan@gmail.com>
2023-07-31 20:44:13 +0500

    Create auth-admin-guard.service.ts

---
+++ intellsihuff-pwa/src/app/modules/admin/auth-admin-guard.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot
+} from '@angular/router';
+import { UserSettingService } from '../user/user-setting.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthAdminGuard implements CanActivate {
+  constructor(
+    private userSettingSvc: UserSettingService,
+    private router: Router
+  ) {}
+
+  async canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ) //  boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree>
+  {
+    const user = <any>await this.userSettingSvc.getCurrentUser();
+    let isAdmin = false;
+    if (user) {
+      user.roles.filter((r): any => {
+        if (r.role.includes('admin')) {
+          isAdmin = true;
+        }
+      });
+      if (isAdmin) {
+        return true;
+      }
+    }
+
+    this.router.navigate(['/403']);
+    return false;
+  }
+}
